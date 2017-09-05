076554066dcd7c6ce278e882409c766bc72fe69e
U-VAIO\Daniel <daniel_sephirot@hotmail.com>
2017-09-05 20:50:22 +0200

    create method loginAction

--- symfony/src/AppBundle/Controller/DefaultController.php
+++ symfony/src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Constraints as Assert;
 use AppBundle\Services\Helpers;
 
 class DefaultController extends Controller
@@ -17,6 +18,49 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+
+    //estos métodos necesitan Action para poder ser accesibles como ruta en la app
+    public function loginAction(Request $request){
+       //Para poder usar nuestro servicio helpers
+       $helpers = $this->get(Helpers::class);
+
+       //Recibir json por POST
+       $json = $request->get('json',null);
+
+
+       if($json !=null){
+           //Convertimos un json a un objeto php
+           $params = json_decode($json);
+
+           //si existe el email le das el valor de params->email si no, se pone a null
+           $email = (isset($params->email)) ? $params->email :null;
+           $password = (isset($params->password)) ? $params->password :null;
+
+           //Validate email
+           $emailConstraint = new Assert\Email();
+           //add message error
+           $emailConstraint->message = "This email is not valid !!";
+           $validate_email = $this->get("validator")->validate($email,$emailConstraint);
+
+           //si es igual a 0 el email se valida correctamente
+           if($email != null && count($validate_email) == 0 && $password !=null){
+            $data = array(
+                'status' => 'ok',
+                'data' => 'Login correct'
+             );
+            }else{
+                $data = array(
+                    'status' => 'error',
+                    'data' => 'Email or password incorrect'
+                );
+            }
+        }
+
+       return $helpers->json($data);
+
+
     }
 
 
